2fa306b9d9b25aa1abcdb54ddf127117dd7efddf
sjosephs <93228965+sjosephs@users.noreply.github.com>
2025-02-26 11:19:12 +0100

    Update components/ShoppingItem.js
    Incorporated props in styled components with $
    
    Co-authored-by: Niklas Röttger <98757391+NikRoe@users.noreply.github.com>

--- components/ShoppingItem.js
+++ components/ShoppingItem.js
@@ -11,7 +11,7 @@ const Article = styled.article`
 `;
 
 const ItemName = styled.p`
-  text-decoration: ${({ purchased }) => (purchased ? "line-through" : "none")};
+  text-decoration: ${({ $purchased }) => ($purchased ? "line-through" : "none")};
   font-weight: bold;
 `;
 
