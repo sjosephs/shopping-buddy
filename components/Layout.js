ed2f7b8d5c5d7da7f6dd0e722133140fa28e508e
Stephanie Preuthen <kann.zaubern@gmail.com>
2025-03-10 11:33:17 +0100

    add favicon icon

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,9 @@ import TitleBar from "./TitleBar";
 import styled from "styled-components";
 import Head from "next/head";
 import { useSession } from "next-auth/react";
+import { faDolly } from "@fortawesome/free-solid-svg-icons";
+
+const faDollySvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="32" height="32"><path d="${faDolly.icon[4]}" /></svg>`;
 
 const Main = styled.main`
   display: grid;
@@ -16,6 +19,10 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
+        <link
+          rel="icon"
+          href={`data:image/svg+xml;base64,${btoa(faDollySvg)}`}
+        />
         <title>Shopping Buddy</title>
       </Head>
       {session && <TitleBar />}
